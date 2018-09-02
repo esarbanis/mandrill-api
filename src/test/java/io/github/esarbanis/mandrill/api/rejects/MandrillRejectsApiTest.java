1ffb2f67ab4752413f12996199892e44b9b418a8
Efthymios Sarmpanis <e.sarbanis@gmail.com>
2018-09-02 21:44:09 +0300

    fix(rejects): sync rejects sub context with mandrill api
    Align the sdk to the api and fix tests

--- src/test/java/io/github/esarbanis/mandrill/api/rejects/MandrillRejectsApiTest.java
+++ src/test/java/io/github/esarbanis/mandrill/api/rejects/MandrillRejectsApiTest.java
@@ -5,6 +5,7 @@ package io.github.esarbanis.mandrill.api.rejects;
 
 import java.io.IOException;
 
+import io.github.esarbanis.mandrill.api.Fixtures;
 import io.github.esarbanis.mandrill.api.MandrillTestCase;
 import junit.framework.Assert;
 
@@ -13,23 +14,28 @@ import org.junit.Test;
 
 import io.github.esarbanis.mandrill.api.common.MandrillApiError;
 
-@Ignore("Need to wire mocks")
 public final class MandrillRejectsApiTest extends MandrillTestCase {
 	
 	@Test(expected=MandrillApiError.class)
 	public final void testAdd() throws IOException, MandrillApiError {
+		mockResponse("/rejects/add.json", 400, Fixtures.Rejects.VALIDATION_ERROR_RESPONSE);
+
 		mandrillApi.rejects().add(null, null, null);
 		Assert.fail();
 	}
 	
 	@Test
 	public final void testList() throws IOException, MandrillApiError {
+		mockResponse("/rejects/list.json", 200, Fixtures.Rejects.LIST_RESPONSE);
+
 		Assert.assertNotNull( mandrillApi.rejects().list(null, null) );
 		
 	}
 	
 	@Test(expected=MandrillApiError.class)
 	public final void testDelete() throws IOException, MandrillApiError {
+		mockResponse("/rejects/delete.json", 400, Fixtures.Rejects.VALIDATION_ERROR_RESPONSE);
+
 		mandrillApi.rejects().delete(null);
 		Assert.fail();
 	}
