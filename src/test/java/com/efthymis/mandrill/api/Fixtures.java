01da400a444fbca1ce609e2999e91bc23eac530c
esarbanis <e.sarbanis@gmail.com>
2018-09-26 15:21:13 +0300

    fix(senders): sync senders sub context with mandrill api
    Align the sdk to the api and fix tests

--- src/test/java/com/efthymis/mandrill/api/Fixtures.java
+++ src/test/java/com/efthymis/mandrill/api/Fixtures.java
@@ -51,8 +51,8 @@ public abstract class Fixtures {
 		public static final String SEND_TEMPLATE_NO_TEMPLATE_ERROR_RESPONSE =
 				"{\"status\":\"error\",\"code\":12,\"name\":\"Unknown_Template\",\"message\":\"The" + " requested template does not exist\"}";
 
-		public static final String SEND_TEMPLATE_NO_SUBACCOUNT_ERROR_RESPONSE = "{\"status\":\"error\",\"code\":12,\"name\":\"Unknown_Subaccount\","
-				+ "\"message\":\"The provided subaccount id does not exist.\"}";
+		public static final String SEND_TEMPLATE_NO_SUBACCOUNT_ERROR_RESPONSE =
+				"{\"status\":\"error\",\"code\":12,\"name\":\"Unknown_Subaccount\"," + "\"message\":\"The provided subaccount id does not exist.\"}";
 
 		public static final String SEARCH_RESPONSE =
 				"[\n" + "    {\n" + "        \"ts\": 1365190000,\n" + "        \"_id\": \"abc123abc123abc123abc123\",\n"
@@ -63,21 +63,20 @@ public abstract class Fixtures {
 						+ "                \"ip\": \"55.55.55.55\",\n" + "                \"location\": \"Georgia, US\",\n"
 						+ "                \"ua\": \"Linux/Ubuntu/Chrome/Chrome 28.0.1500.53\"\n" + "            }\n" + "        ],\n"
 						+ "        \"clicks\": 42,\n" + "        \"clicks_detail\": [\n" + "            {\n" + "                \"ts\": "
-						+ "1365190001,\n"
-						+ "                \"url\": \"http://www.example.com\",\n" + "                \"ip\": \"55.55.55.55\",\n"
+						+ "1365190001,\n" + "                \"url\": \"http://www.example.com\",\n" + "                \"ip\": \"55.55.55.55\",\n"
 						+ "                \"location\": \"Georgia, US\",\n" + "                \"ua\": \"Linux/Ubuntu/Chrome/Chrome 28.0.1500"
-						+ ".53\"\n"
-						+ "            }\n" + "        ],\n" + "        \"state\": \"sent\",\n" + "        \"metadata\": {\n"
+						+ ".53\"\n" + "            }\n" + "        ],\n" + "        \"state\": \"sent\",\n" + "        \"metadata\": {\n"
 						+ "            \"user_id\": \"123\",\n" + "            \"website\": \"www.example.com\"\n" + "        }\n" + "    }\n" + "]";
 
-		public static final String CONTENT_RESPONSE = "{\n" + "    \"ts\": 1365190000,\n" + "    \"_id\": \"abc123abc123abc123abc123\",\n"
-				+ "    \"from_email\": \"sender@example.com\",\n" + "    \"from_name\": \"Sender Name\",\n"
-				+ "    \"subject\": \"example subject\",\n" + "    \"to\": {\n" + "        \"email\": \"recipient.email@example.com\",\n"
-				+ "        \"name\": \"Recipient Name\"\n" + "    },\n" + "    \"tags\": [\n" + "        \"password-reset\"\n" + "    ],\n"
-				+ "    \"headers\": {\n" + "        \"Reply-To\": \"replies@example.com\"\n" + "    },\n" + "    \"text\": \"Some text content\",\n"
-				+ "    \"html\": \"<p>Some HTML content</p>\",\n" + "    \"attachments\": [\n" + "        {\n"
-				+ "            \"name\": \"example.txt\",\n" + "            \"type\": \"text/plain\",\n"
-				+ "            \"content\": \"QSBzaW1wbGUgdGV4dCBzdHJpbmcgYXR0YWNobWVudA==\"\n" + "        }\n" + "    ]\n" + "}";
+		public static final String CONTENT_RESPONSE =
+				"{\n" + "    \"ts\": 1365190000,\n" + "    \"_id\": \"abc123abc123abc123abc123\",\n" + "    \"from_email\": \"sender@example"
+						+ ".com\",\n" + "    \"from_name\": \"Sender Name\",\n" + "    \"subject\": \"example subject\",\n" + "    \"to\": {\n"
+						+ "        \"email\": \"recipient.email@example.com\",\n" + "        \"name\": \"Recipient Name\"\n" + "    },\n"
+						+ "    \"tags\": [\n" + "        \"password-reset\"\n" + "    ],\n" + "    \"headers\": {\n"
+						+ "        \"Reply-To\": \"replies@example.com\"\n" + "    },\n" + "    \"text\": \"Some text content\",\n"
+						+ "    \"html\": \"<p>Some HTML content</p>\",\n" + "    \"attachments\": [\n" + "        {\n"
+						+ "            \"name\": \"example.txt\",\n" + "            \"type\": \"text/plain\",\n"
+						+ "            \"content\": \"QSBzaW1wbGUgdGV4dCBzdHJpbmcgYXR0YWNobWVudA==\"\n" + "        }\n" + "    ]\n" + "}";
 
 		public static final String PARSE_RESPONSE = "{\"subject\":\"Some Subject\",\"from_email\":\"sender@example.com\",\"from_name\":\"Sender "
 				+ "Name\",\"to\":[{\"email\":\"recipient.email@example.com\",\"name\":\"Recipient Name\"}],"
@@ -85,11 +84,12 @@ public abstract class Fixtures {
 				+ "\"attachments\":[{\"name\":\"example.txt\",\"type\":\"text/plain\",\"binary\":false,\"content\":\"example non-binary content\"}],"
 				+ "\"images\":[{\"name\":\"IMAGEID\",\"type\":\"image/png\",\"content\":\"ZXhhbXBsZSBmaWxl\"}]}";
 
-		public static final String PARSE_ERROR_RESPONSE = "{\"status\":\"error\",\"code\":-1,\"name\":\"ValidationError\",\"message\":\"You must "
-				+ "specify a raw_message value\"}";
+		public static final String PARSE_ERROR_RESPONSE =
+				"{\"status\":\"error\",\"code\":-1,\"name\":\"ValidationError\",\"message\":\"You must " + "specify a raw_message value\"}";
 	}
 
 	public static class Tags {
+
 		public static final String LIST_RESPONSE = "[{\"tag\":\"example-tag\",\"reputation\":42,\"sent\":42,\"hard_bounces\":42,\"soft_bounces\":42,"
 				+ "\"rejects\":42,\"complaints\":42,\"unsubs\":42,\"opens\":42,\"clicks\":42,\"unique_opens\":42,\"unique_clicks\":42}]";
 
@@ -126,7 +126,33 @@ public abstract class Fixtures {
 
 	public static class Whitelists {
 
-		public static final String LIST_RESPONSE = "[{\"email\":\"example email\",\"detail\":\"Whitelisted internal address\","
-				+ "\"created_at\":\"2013-01-01 15:30:32\"}]";
+		public static final String LIST_RESPONSE =
+				"[{\"email\":\"example email\",\"detail\":\"Whitelisted internal address\"," + "\"created_at\":\"2013-01-01 15:30:32\"}]";
+	}
+
+	public static class Senders {
+
+		public static final String LIST_RESPONSE = "[{\"address\":\"sender.example@mandrillapp.com\",\"created_at\":\"2013-01-01 15:30:27\","
+				+ "\"sent\":42,\"hard_bounces\":42,\"soft_bounces\":42,\"rejects\":42,\"complaints\":42,\"unsubs\":42,\"opens\":42,\"clicks\":42,"
+				+ "\"unique_opens\":42,\"unique_clicks\":42}]";
+
+		public static final String DOMAINS_RESPONSE = "[{\"domain\":\"example.com\",\"created_at\":\"2013-01-01 15:30:27\","
+				+ "\"last_tested_at\":\"2013-01-01 15:40:42\",\"spf\":{\"valid\":true,\"valid_after\":\"2013-01-01 15:45:23\",\"error\":\"example "
+				+ "error\"},\"dkim\":{\"valid\":true,\"valid_after\":\"2013-01-01 15:45:23\",\"error\":\"example error\"},"
+				+ "\"verified_at\":\"2013-01-01 15:50:21\",\"valid_signing\":true}]";
+
+		public static final String INFO_RESPONSE = "{\"address\":\"sender.example@mandrillapp.com\",\"created_at\":\"2013-01-01 15:30:27\","
+				+ "\"sent\":42,\"hard_bounces\":42,\"soft_bounces\":42,\"rejects\":42,\"complaints\":42,\"unsubs\":42,\"opens\":42,\"clicks\":42,"
+				+ "\"stats\":{\"today\":{\"sent\":42,\"hard_bounces\":42,\"soft_bounces\":42,\"rejects\":42,\"complaints\":42,\"unsubs\":42,"
+				+ "\"opens\":42,\"unique_opens\":42,\"clicks\":42,\"unique_clicks\":42},\"last_7_days\":{\"sent\":42,\"hard_bounces\":42,"
+				+ "\"soft_bounces\":42,\"rejects\":42,\"complaints\":42,\"unsubs\":42,\"opens\":42,\"unique_opens\":42,\"clicks\":42,"
+				+ "\"unique_clicks\":42},\"last_30_days\":{\"sent\":42,\"hard_bounces\":42,\"soft_bounces\":42,\"rejects\":42,\"complaints\":42,"
+				+ "\"unsubs\":42,\"opens\":42,\"unique_opens\":42,\"clicks\":42,\"unique_clicks\":42},\"last_60_days\":{\"sent\":42,"
+				+ "\"hard_bounces\":42,\"soft_bounces\":42,\"rejects\":42,\"complaints\":42,\"unsubs\":42,\"opens\":42,\"unique_opens\":42,"
+				+ "\"clicks\":42,\"unique_clicks\":42},\"last_90_days\":{\"sent\":42,\"hard_bounces\":42,\"soft_bounces\":42,\"rejects\":42,"
+				+ "\"complaints\":42,\"unsubs\":42,\"opens\":42,\"unique_opens\":42,\"clicks\":42,\"unique_clicks\":42}}}";
+
+		public static final String TIME_SERIES_RESPONSE = "[{\"time\":\"2013-01-01 15:30:27\",\"sent\":42,\"hard_bounces\":42,\"soft_bounces\":42,"
+				+ "\"rejects\":42,\"complaints\":42,\"opens\":42,\"unique_opens\":42,\"clicks\":42,\"unique_clicks\":42}]";
 	}
 }
--- src/test/java/com/efthymis/mandrill/api/senders/MandrillSendersApiTest.java
+++ src/test/java/com/efthymis/mandrill/api/senders/MandrillSendersApiTest.java
@@ -1,11 +1,13 @@
 /**
- * 
+ *
  */
 package com.efthymis.mandrill.api.senders;
 
 import java.io.IOException;
 
+import com.efthymis.mandrill.api.Fixtures;
 import com.efthymis.mandrill.api.MandrillTestCase;
+
 import junit.framework.Assert;
 
 import org.junit.Ignore;
@@ -14,68 +16,71 @@ import org.junit.Test;
 import com.efthymis.mandrill.api.common.MandrillApiError;
 import com.efthymis.mandrill.api.urls.MandrillTimeSeries;
 
-@Ignore("Need to wire mocks")
 public final class MandrillSendersApiTest extends MandrillTestCase {
-	
+
 	@Test
 	public final void testList() throws IOException, MandrillApiError {
+		mockResponse("/senders/list.json", 200, Fixtures.Senders.LIST_RESPONSE);
+
 		final MandrillSender[] senders = mandrillApi.senders().list();
-		Assert.assertNotNull( senders );
-		for(MandrillSender s : senders) {
+		Assert.assertNotNull(senders);
+		for (MandrillSender s : senders) {
 			Assert.assertNotNull(s.getAddress());
 		}
 	}
-	
+
 	@Test
 	public final void testDomains() throws IOException, MandrillApiError {
+		mockResponse("/senders/domains.json", 200, Fixtures.Senders.DOMAINS_RESPONSE);
+
 		final MandrillDomain[] domains = mandrillApi.senders().domains();
 		Assert.assertNotNull(domains);
-		for(MandrillDomain d : domains) {
+		for (MandrillDomain d : domains) {
 			Assert.assertNotNull(d.getDomain());
 		}
 	}
-	
-	@Test(expected=MandrillApiError.class)
+
+	@Test(expected = MandrillApiError.class)
 	public final void testAddDomain() throws IOException, MandrillApiError {
+		mockResponse("/senders/add-domain.json", 400, Fixtures.VALIDATION_ERROR_RESPONSE);
+
 		mandrillApi.senders().addDomain(null);
 	}
-	
-	@Test(expected=MandrillApiError.class)
+
+	@Test(expected = MandrillApiError.class)
 	public final void testInfo01() throws IOException, MandrillApiError {
+		mockResponse("/senders/info.json", 400, Fixtures.VALIDATION_ERROR_RESPONSE);
+
 		mandrillApi.senders().info(null);
 		Assert.fail();
 	}
-	
+
 	@Test
 	public final void testInfo02() throws IOException, MandrillApiError {
-		final MandrillSender[] senders = mandrillApi.senders().list();
-		Assert.assertNotNull( senders );
-		if(senders.length > 0) {
-			final MandrillSender s = mandrillApi.senders().info(
-					senders[0].getAddress());
-			Assert.assertNotNull(s);
-			Assert.assertNotNull(s.getAddress());
-			Assert.assertNotNull(s.getCreatedAt());
-		}
+		mockResponse("/senders/info.json", 200, Fixtures.Senders.INFO_RESPONSE);
+
+		final MandrillSender s = mandrillApi.senders().info("sender.example@mandrillapp.com");
+		Assert.assertNotNull(s);
+		Assert.assertNotNull(s.getAddress());
+		Assert.assertNotNull(s.getCreatedAt());
 	}
-	
-	@Test(expected=MandrillApiError.class)
+
+	@Test(expected = MandrillApiError.class)
 	public final void testTimeSeries01() throws MandrillApiError, IOException {
+		mockResponse("/senders/time-series.json", 400, Fixtures.VALIDATION_ERROR_RESPONSE);
+
 		mandrillApi.senders().timeSeries(null);
 		Assert.fail();
 	}
-	
+
 	@Test
 	public final void testTimeSeries02() throws MandrillApiError, IOException {
-		final MandrillSender[] senders = mandrillApi.senders().list();
-		Assert.assertNotNull( senders );
-		if(senders.length > 0) {
-			final MandrillTimeSeries[] series =
-					mandrillApi.senders().timeSeries(senders[0].getAddress());
-			Assert.assertNotNull(series);
-			for(MandrillTimeSeries s : series) {
-				Assert.assertNotNull(s.getTime());
-			}
+		mockResponse("/senders/time-series.json", 200, Fixtures.Senders.TIME_SERIES_RESPONSE);
+
+		final MandrillTimeSeries[] series = mandrillApi.senders().timeSeries("sender.example@mandrillapp.com");
+		Assert.assertNotNull(series);
+		for (MandrillTimeSeries s : series) {
+			Assert.assertNotNull(s.getTime());
 		}
 	}
 
